31f36e305e3cb405ca168ea39494fc59b9ad9158
Manju-002 <manjuchoudhuri24@gmail.com>
2024-08-22 00:34:09 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,123 @@
+//creating an array and passing te number , questions ,options ,and answers
+let questions = [
+{
+    numb: 1,
+    image:"disgust.png",
+    answers : " Disgusted ",
+    answer: " Disgusted" ,
+    options: [
+        "Angry",
+        " Happy",
+        " Sad",
+        " Disgusted"
+    ]
+},
+{
+    numb: 2,
+    image: "angry.png",
+    answers : "Angry",
+    answer: " Angry",
+    options: [
+        " Angry",
+        " Fearful",
+        " Sad",
+        " Disgusted"
+    ]
+},
+{
+    numb: 3,
+    image: "fear1.png",
+    answers: "Fearful",
+    answer:  " Fearful",
+    options: [
+        "Angry",
+        " Happy",
+        " Fearful",
+        " Disgusted"
+    ]
+},
+{
+    numb: 4,
+    image:"nutral.png",
+    answers : "Neutral",
+    answer: "Neutral",
+    options: [
+        "Neutral",
+        " Happy",
+        " Sad",
+        " Disgusted"
+    ]
+},
+{
+    numb: 5,
+    image:"happy.png",
+    answers: "Happy",
+    answer: " Happy",
+    options: [
+        "Angry",
+        " Happy",
+        " Sad",
+        " Disgusted"
+    ]
+},
+{
+    numb: 6,
+    image:"sad.png",
+    answers: "sad",
+    answer:  " Sad",
+    options: [
+        " Happy",
+        " Sad",
+        " Angry",
+        " Neutral"
+    ]
+},
+{
+    numb: 7,
+    image:"nutral (2).png",
+    answers: "Neutral",
+    answer:  "Neutral",
+    options: [
+        "Neutral",
+        " Fearful",
+        " Sad",
+        " Disgusted"
+    ]
+},
+{
+    numb: 8,
+    image:"suprise.png",
+    answers: "Suprised",
+    answer: " Suprised",
+    options: [
+        "Angry",
+        " Suprised",
+        " Sad",
+        " Disgusted"
+    ]
+},
+{
+    numb: 9,
+    image:"disgust1.png",
+    answers: "Disgusted",
+    answer:  " Disgusted",
+    options: [
+        "Angry",
+        " Happy",
+        " Fearful",
+        " Disgusted"
+    ]
+},
+{
+    numb: 10,
+    image:" happy2.png",
+    answers: "Happy",
+    answer: " Happy",
+    options: [
+        "Angry",
+        " Happy",
+        " Sad",
+        " Disgusted"
+    ]
+},
+];
